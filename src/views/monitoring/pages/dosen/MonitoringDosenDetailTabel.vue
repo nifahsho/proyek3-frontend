575cb8d8597f826f470fdd65409a1476ef781fb5
HanifahSho <hanifah.sholihat.tif418@polban.ac.id>
2021-06-09 21:11:17 +0700

    update table

--- src/views/monitoring/pages/dosen/MonitoringDosenDetailTabel.vue
+++ src/views/monitoring/pages/dosen/MonitoringDosenDetailTabel.vue
@@ -6,58 +6,31 @@
         <v-col cols="12">
              <breadcumbs :breadcrumb-items="breadcrumbItems"/>
         </v-col>
-         <v-col  class="ml-auto pl-13 py-4" cols="0" xs="6" sm="6" md="5" lg="4" xl="3">
-            <v-combobox solo  v-model="selected" :items="choices" label="181524012 Alvira Putrina D" :dark="isDark"></v-combobox>
-        </v-col>
-        <v-col cols="12">
-        <v-simple-table>
-            <template v-slot:default>
-                <thead>
-                    <tr :style="{background: currentTheme.colorPrimary}">
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Selesai</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Nama</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Target</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Skala Pemahaman</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Durasi</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Catatan</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Lampiran</div>
-                        </th>
-                    </tr>
-                </thead>
-            <tbody :style="{background: currentTheme.surface}">
-                <tr
-                    v-for="subTask in SubTaskList"
-                    :key="subTask.selesai"
-                    :style="{pointerEvents: 'none' }"
-                    align-center
-                >
-                    <td class="text-capitalize text-body-2 font-weight-light text-center test" :style="{color: currentTheme.onSurface}">{{ subTask.selesai}}
-                        <v-checkbox></v-checkbox>
-                    </td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-left" :style="{color: currentTheme.onSurface}">{{ subTask.nama}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.target}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.skalapemahaman}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.durasi}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.catatan}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.lampiran}}</td>
-                </tr>
-             </tbody>
-        </template>
-    </v-simple-table>
-    </v-col>
+         <v-col  class="ml-auto pl-13 py-4" cols="12" xs="6" sm="12" md="4" lg="4" xl="3">
+           <v-combobox solo v-model="select" :items="choices" :dark="isDark" item-color="#C4C4C4" dense>
+            </v-combobox>
+         </v-col>
+        <v-data-table
+          dark
+          :headers="headers"
+          :items="items"
+          :loading="isLoading"
+          loading-text=""
+          :items-per-page="5"
+          class="elevation-1"
+          :style="{backgroundColor: currentTheme.onBackground}"
+        >
+        <template v-slot:no-data>
+      <p
+        :style="{color: currentTheme.colorPrimary}"
+        class="text-lg-subtitle-1 font-weight-bold"
+      >No Data Available
+      </p>
+    </template>
+    <template  v-slot:[`item.Selesai`]="{ item }"  >
+        <v-simple-checkbox class="t" v-model="item.Selesai" disabled></v-simple-checkbox>
+      </template>
+  </v-data-table>
     </v-row>
 </template>
 
@@ -68,89 +41,13 @@ import Breadcumbs from "@/views/shared/navigation/Breadcumbs"
 export default {
   name: "DosenViewTable",
   components: { Breadcumbs },
-  props: {
-    SubTaskList: {
-      type: Array,
-      required: false,
-      default: () => {
-        return [
-          {
-            nama: "Another Type of Employee",
-            target: "50",
-            skalapemahaman: "30",
-            durasi: "00:30:00",
-            catatan: "",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Another Type of Employee",
-            target: "50",
-            skalapemahaman: "30",
-            durasi: "00:30:00",
-            catatan: "Dalam kasus ini multi-Thread, ketika program di run hasilnya kata Hello JTK 2018 tidak beraturan,ini dikarenakan thread..",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Planning Shapes",
-            target: "50",
-            skalapemahaman: "30",
-            durasi: "00:30:00",
-            catatan: "",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Polymorphic Sorting",
-            target: "50",
-            skalapemahaman: "30",
-            durasi: "00:30:00",
-            catatan: "",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Timming Searching and Sorting Algorithms",
-            target: "50",
-            skalapemahaman: "30",
-            durasi: "00:30:00",
-            catatan: "",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Coloring a Moveaible Circle",
-            target: "80",
-            skalapemahaman: "7",
-            durasi: "00:50:00",
-            catatan: "",
-            lampiran: "Link Spreadsheet"
-          },
-          {
-            nama: "Speed Control",
-            target: "100",
-            skalapemahaman: "6",
-            durasi: "01:02:00",
-            catatan: "Dalam kasus ini multi-Thread, ketika program di run hasilnya kata Hello JTK 2018 tidak beraturan,ini dikarenakan thread..",
-            lampiran: "Link Spreadsheet"
-          }
-        ]
-      }
-    }
-  },
   computed: {
     ...mapGetters({
       currentTheme: "theme/getCurrentColor"
-    }),
-    isMobile () {
-      return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs
-    },
-    tableTitleStyle () {
-      return {
-        color: this.currentTheme.colorOnPrimary
-      }
-    }
+    })
   },
   data () {
     return {
-      selected: null,
-      choices: ["181524002 Alvira Putrina D", "181524004 Ananda Bayu F", "181524004 Chofief Al Farroqie A"],
       breadcrumbItems: [
         {
           text: "Monitoring",
@@ -167,11 +64,135 @@ export default {
           disabled: true,
           href: ""
         }
-      ]
+      ],
+      select: ["181524002-Alvira Putrina D"],
+      choices: [
+        "181524003-Ananda Bayu F",
+        "181524004-Chofief Al Farroqie",
+        "181524005-Dewanto Joyo P",
+        "181524006-Dwinanda Alfauzan S"
+      ],
+      headers: [
+        {
+          text: "Selesai",
+          align: "center",
+          value: "Selesai",
+          sortable: false,
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Nama",
+          value: "Nama",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Skala Pemahaman",
+          align: "center",
+          value: "SkalaPemahaman",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Durasi",
+          align: "center",
+          value: "Durasi",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Catatan",
+          value: "Catatan",
+          align: "center",
+          sortable: false,
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Lampiran",
+          value: "Lampiran",
+          align: "center",
+          sortable: false,
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        }
+      ],
+      items: [
+        {
+          Selesai: true,
+          Nama: "Another Type of Employee",
+          Target: "50",
+          SkalaPemahaman: "4.5",
+          Durasi: "00:30:00",
+          Catatan: "",
+          Lampiran: "Link Spreadsheet"
+        },
+        {
+          Selesai: true,
+          Nama: "Another Type of Employee",
+          Target: "50",
+          SkalaPemahaman: "4.7",
+          Durasi: "00:30:00",
+          Catatan: "Dalam kasus ini multi-Thread, ketika program di run hasilnya kata Hello JTK 2018 tidak beraturan,ini dikarenakan thread..",
+          Lampiran: "Link Dokumen"
+        },
+        {
+          Selesai: false,
+          Nama: "Planning Shapes",
+          Target: "50",
+          SkalaPemahaman: "5",
+          Durasi: "00:30:00",
+          Catatan: "",
+          Lampiran: "Link Dokumen"
+        },
+        {
+          Selesai: true,
+          Nama: "Polymorphic Sorting",
+          Target: "50",
+          SkalaPemahaman: "4.5",
+          Durasi: "00:30:00",
+          Catatan: "",
+          Lampiran: "Link Dokumen"
+        },
+        {
+          Selesai: true,
+          Nama: "Timming Searching and Sorting Algorithms",
+          Target: "50",
+          SkalaPemahaman: "5",
+          Durasi: "00:30:00",
+          Catatan: "",
+          Lampiran: "Link Dokumen"
+        },
+        {
+          Selesai: false,
+          Nama: "Coloring a Moveaible Circle",
+          Target: "80",
+          SkalaPemahaman: "4.2",
+          Durasi: "00:50:00",
+          Catatan: "",
+          Lampiran: "Link Spreadsheet"
+        },
+        {
+          Selesai: false,
+          Nama: "Speed Control",
+          Target: "100",
+          SkalaPemahaman: "5",
+          Durasi: "01:02:00",
+          Catatan: "Dalam kasus ini multi-Thread, ketika program di run hasilnya kata Hello JTK 2018 tidak beraturan,ini dikarenakan thread..",
+          Lampiran: "Link Spreadsheet"
+        }
+      ],
+      isLoading: false,
+      rulesFile: {
+      }
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+  tbody {
+    tr:hover {
+      background-color: #b9b9b9 !important;
+    }
+    tr {
+      color: black;
+      background-color: #FFFFFF !important;
+    }
+  }
 </style>
--- src/views/monitoring/pages/dosen/MonitoringDosenTableTugas.vue
+++ src/views/monitoring/pages/dosen/MonitoringDosenTableTugas.vue
@@ -1,53 +1,40 @@
 <template>
-    <v-row :style="{color: currentTheme.onBackground}">
-        <v-col cols="12">
-            <p class="text-h4 font-weight-bold">Monitoring APPL 1 - W1 Polymorphism</p>
-        </v-col>
-        <v-col cols="12">
-             <breadcumbs :breadcrumb-items="breadcrumbItems"/>
-        </v-col>
-        <v-simple-table>
-            <template v-slot:default>
-                <thead>
-                    <tr :style="{background: currentTheme.colorPrimary}">
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">NIM</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Nama</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Rata-rata Progress</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Taks Dikerjakan</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Total Durasi</div>
-                        </th>
-                        <th class="text-center">
-                            <div :style="tableTitleStyle" class="text-subtitle-2">Detail</div>
-                        </th>
-                    </tr>
-                </thead>
-            <tbody :style="{background: currentTheme.surface}">
-                <tr
-                    v-for="subTask in SubTaskList"
-                    :key="subTask.selesai"
-                    :style="{pointerEvents: 'none' }"
-                    align-center
-                >
-                    <td class="text-capitalize text-body-2 font-weight-light text-center test" :style="{color: currentTheme.onSurface}">{{ subTask.Nim }}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-left" :style="{color: currentTheme.onSurface}">{{ subTask.Nama}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.RataRataProgress}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.TaksDikerjakan}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.TotalDurasi}}</td>
-                    <td class="text-capitalize text-body-2 font-weight-light text-center" :style="{color: currentTheme.onSurface}">{{ subTask.Detail}}</td>
-                </tr>
-             </tbody>
-        </template>
-    </v-simple-table>
-    </v-row>
+<v-container>
+  <v-row :style="{color: currentTheme.onBackground}">
+ <v-col cols="12">
+        <p class="text-h4 font-weight-bold">Monitoring APPL 1 - W1 Polymorphism</p>
+    </v-col>
+    <v-col cols="12">
+        <breadcumbs :breadcrumb-items="breadcrumbItems"/>
+    </v-col>
+  <v-data-table
+    dark
+    :headers="headers"
+    :items="items"
+    :loading="isLoading"
+    loading-text=""
+    :items-per-page="5"
+    class="elevation-1"
+    :style="{backgroundColor: currentTheme.onBackground}"
+  >
+    <template v-slot:no-data>
+      <p
+        :style="{color: currentTheme.colorPrimary}"
+        class="text-lg-subtitle-1 font-weight-bold"
+      >No Data Available
+      </p>
+    </template>
+    <template v-slot:[`item.Detail`]>
+    <v-btn
+      text
+      color="primary"
+    >
+      Lihat Detail
+    </v-btn>
+     </template>
+  </v-data-table>
+</v-row>
+</v-container>
 </template>
 
 <script>
@@ -55,118 +42,12 @@ import { mapGetters } from "vuex"
 import Breadcumbs from "@/views/shared/navigation/Breadcumbs"
 
 export default {
-  name: "DosenViewTable",
+  name: "KelasItem",
   components: { Breadcumbs },
-  props: {
-    SubTaskList: {
-      type: Array,
-      required: false,
-      default: () => {
-        return [
-          {
-            Nim: "181524002",
-            Nama: "Alvira Putrina Derajat",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "3",
-            TotalDurasi: "250",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524003",
-            Nama: "Ananda Bayu Fauzan",
-            RataRataProgress: "60%",
-            TaksDikerjakan: "5",
-            TotalDurasi: "300",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524004",
-            Nama: "Chofief Al Farroqie Ariestotles",
-            RataRataProgress: "60%",
-            TaksDikerjakan: "4",
-            TotalDurasi: "270",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524005",
-            Nama: "Dewanto Joyo Pramono",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "4",
-            TotalDurasi: "240",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524006",
-            Nama: "Dwinanda Alfauzan Suhando",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "3",
-            TotalDurasi: "150",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524007",
-            Nama: "Evan Lokajaya",
-            RataRataProgress: "70%",
-            TaksDikerjakan: "5",
-            TotalDurasi: "340",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524008",
-            Nama: "Fajrina Aflaha",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "4",
-            TotalDurasi: "300",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524009",
-            Nama: "Fatharani",
-            RataRataProgress: "60%",
-            TaksDikerjakan: "5",
-            TotalDurasi: "300",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524010",
-            Nama: "Hafizh Muhammad F",
-            RataRataProgress: "60%",
-            TaksDikerjakan: "3",
-            TotalDurasi: "300",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524012",
-            Nama: "Hanifah Sholihat",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "2",
-            TotalDurasi: "200",
-            Detail: "Lihat Detail"
-          },
-          {
-            Nim: "181524012",
-            Nama: "Hanifah Sholihat",
-            RataRataProgress: "50%",
-            TaksDikerjakan: "2",
-            TotalDurasi: "200",
-            Detail: "Lihat Detail"
-          }
-        ]
-      }
-    }
-  },
   computed: {
     ...mapGetters({
       currentTheme: "theme/getCurrentColor"
-    }),
-    isMobile () {
-      return this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs
-    },
-    tableTitleStyle () {
-      return {
-        color: this.currentTheme.colorOnPrimary
-      }
-    }
+    })
   },
   data () {
     return {
@@ -186,11 +67,134 @@ export default {
           disabled: true,
           href: ""
         }
-      ]
+      ],
+      headers: [
+        {
+          text: "NIM",
+          align: "center",
+          value: "Nim",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Nama",
+          value: "Nama",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Rata-Rata Progress",
+          align: "center",
+          value: "RataRataProgress",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Taks Dikerjakan",
+          align: "center",
+          value: "TaksDikerjakan",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Total Durasi",
+          align: "center",
+          value: "TotalDurasi",
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        },
+        {
+          text: "Detail",
+          value: "Detail",
+          align: "center",
+          sortable: false,
+          class: "white--text text-lg-subtitle-1 font-weight-bold"
+        }
+      ],
+      items: [
+        {
+          Nim: "181524002",
+          Nama: "Alvira Putrina Derajat",
+          RataRataProgress: "50%",
+          TaksDikerjakan: "3",
+          TotalDurasi: "250"
+        },
+        {
+          Nim: "181524003",
+          Nama: "Ananda Bayu Fauzan",
+          RataRataProgress: "60%",
+          TaksDikerjakan: "5",
+          TotalDurasi: "300"
+        },
+        {
+          Nim: "181524004",
+          Nama: "Chofief Al Farroqie Ariestotles",
+          RataRataProgress: "60%",
+          TaksDikerjakan: "4",
+          TotalDurasi: "270"
+        },
+        {
+          Nim: "181524005",
+          Nama: "Dewanto Joyo Pramono",
+          RataRataProgress: "50%",
+          TaksDikerjakan: "4",
+          TotalDurasi: "240"
+        },
+        {
+          Nim: "181524006",
+          Nama: "Dwinanda Alfauzan Suhando",
+          RataRataProgress: "50%",
+          TaksDikerjakan: "3",
+          TotalDurasi: "150"
+        },
+        {
+          Nim: "181524007",
+          Nama: "Evan Lokajaya",
+          RataRataProgress: "70%",
+          TaksDikerjakan: "5",
+          TotalDurasi: "340"
+        },
+        {
+          Nim: "181524008",
+          Nama: "Fajrina Aflaha",
+          RataRataProgress: "50%",
+          TaksDikerjakan: "4",
+          TotalDurasi: "300",
+          Detail: "Lihat Detail"
+        },
+        {
+          Nim: "181524009",
+          Nama: "Fatharani",
+          RataRataProgress: "60%",
+          TaksDikerjakan: "5",
+          TotalDurasi: "300"
+        },
+        {
+          Nim: "181524010",
+          Nama: "Hafizh Muhammad F",
+          RataRataProgress: "60%",
+          TaksDikerjakan: "3",
+          TotalDurasi: "300"
+        },
+        {
+          Nim: "181524012",
+          Nama: "Hanifah Sholihat",
+          RataRataProgress: "50%",
+          TaksDikerjakan: "2",
+          TotalDurasi: "200"
+        }
+      ],
+      isLoading: false,
+      rulesFile: {
+      }
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+  tbody {
+    tr:hover {
+      background-color: #b9b9b9 !important;
+    }
+    tr {
+      color: black;
+      background-color: #FFFFFF !important;
+    }
+  }
 </style>
